a6682a8efa55cb19275fc374646465b0cb654f4a
Tomáš Zellerin <tomas@zellerin.cz>
2021-12-12 09:04:44 +0100

    Documen save-value/load-value.

--- tz-utilities.lisp
+++ tz-utilities.lisp
@@ -1,10 +1,9 @@
 (in-package #:tz-utilities)
 
 (define-section @small-utils
-  "Random convenience utils related to alists, authinfo, and symbols names."
+  "Random convenience utils related to alists, and symbols names."
   (assocd) (assocd-for)
-  (clist-to-llist)
-  (save-value) (load-value))
+  (clist-to-llist))
 
 (declaim (inline assocd assocd-for))
 
@@ -22,21 +21,46 @@ FIXME: this must be in some standard library, but I can't find it."
   (e.g., used for display in org mode)."
   (mapcar (lambda (a) (list (car a) (cdr a))) object))
 
-(defun save-value (value base-name &key (cache-path "~/.cache/lisp/"))
+(define-section @save-load
+  "To preserve relatively stable data across run of the system, the
+  actual values of the variables can be saved and then loaded on
+  startup (or anytime else).
+
+  The data are saved to a define directory with names created from the
+  provided string and timestamp; loading is done from the most recent
+  (based on name) file.
+
+  The background mechanism for storing is cl-store.
+
+  Typical sequence is
+
+  (defvar *A-VARIABLE* (load-value \"foo\"))
+  ...
+  ;;; some long calculation to update *A-VARIABLE*
+  ...
+  (save-value \"foo\")"
+  (save-value)
+  (load-value)
+  (*default-cache-path* variable))
+
+(defvar *default-cache-path* "~/.cache/lisp/")
+
+(defun save-value (value base-name &key (cache-path *default-cache-path*))
   "Store value to a timestamped storage in cache."
   (cl-store:store value
 		  (ensure-directories-exist
 		   (local-time:format-timestring nil  (local-time:now)
 						 :format `(,cache-path ,base-name
-									      "-" ,@local-time:+iso-8601-date-format+ ".store"))))
+								       "-" ,@local-time:+iso-8601-date-format+ ".store"))))
   (values))
 
 
-(defun load-value (base-name &key (cache-path "~/.cache/lisp/"))
+(defun load-value (base-name &key (cache-path *default-cache-path*))
+  "Load value keyed by BASE-NAME from the most recent file (by name) in cache."
   (let* ((most-recent-dump (car
 			    (sort
-			     (print (directory
-				     (format nil "~a~a*.store" cache-path base-name)))
+			     (directory
+			      (format nil "~a~a*.store" cache-path base-name))
 			     #'string<
 			     :key #'pathname-name))))
     (when (null most-recent-dump)
