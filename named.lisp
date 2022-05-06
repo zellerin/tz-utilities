df4fcd3898f9e457a57e95ec31fb3c766cfe6266
Tomáš Zellerin <tomas@zellerin.cz>
2022-05-06 12:41:09 +0200

    Document named objects. Let undefined name be object itself.

--- named.lisp
+++ named.lisp
@@ -1,19 +1,33 @@
 (in-package tz-utilities)
 
+(define-section @named-objects
+  ""
+  (make-json-object))
+
 (defvar *cntr* 0
   "Counter for naming unnamed instances.")
 
 (export-classes (:slots)
   (defclass named ()
     ((name :accessor get-name :initarg :name
-	   :initform (let ((name (format nil "Unnamed instance #~x" (incf *cntr*))))
-		       (cerror "Keep generated name for the object"
-			       "Should have name: ~a" name)
-		       name)))
+	   :initform nil))
     (:documentation
      "Use as a mixin to ensure that the class has slot NAME that is
      used to print it."))
 
-  (defmethod print-object ((o named) stream)
-    (print-unreadable-object (o stream :type t :identity nil)
-      (princ (get-name o) stream))))
+  (defclass named-json (named)
+    ((json :accessor get-json :initarg :json))
+    (:documentation "Base class for objects backed up/created from json. Initialize appropriate slots (such as name) in shared-initialize (or initialize-instance).")))
+
+(defmethod print-object ((o named) stream)
+  (print-unreadable-object (o stream :type t :identity nil)
+    (princ (get-name o) stream)))
+
+(defun make-json-object (json class
+			 &key (name-keyword :name)
+			   (name-fn (assocd-for name-keyword)))
+  (make-instance class :json json :name (funcall name-fn json)))
+
+(defmethod get-name (o)
+  "If we have nothing else, let name be object itself."
+  o)
