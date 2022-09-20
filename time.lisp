be3e87110d78810863b970838f7fee011ba1ee65
Tomáš Zellerin <tomas@zellerin.cz>
2022-09-20 21:38:15 +0200

    (feat) Define readtable local-time.

--- time.lisp
+++ time.lisp
@@ -3,9 +3,16 @@
 (define-section @time-tools
   "Time related utilities. The baseline and convention is the LOCAL-TIME package."
   (tomorrow)
-  (*org-time-format* variable))
+  (*org-time-format* variable)
+  (local-time readtable))
 
-(defun tomorrow (&optional (n 0))
+(named-readtables:defreadtable local-time
+  "@ reads timestring, #@ universal time."
+  (:merge :standard)
+  (:macro-char #\@ 'local-time::%read-timestring)
+  (:dispatch-macro-char #\# #\@ 'local-time::%read-universal-time))
+
+(defun tomorrow (&optional (n 0) (base (now)))
   "Timestamp of Nth day in future (0 being tomorrow) 00:00."
   (let ((result (now)))
     (setf (sec-of result) (* 3600 24))
