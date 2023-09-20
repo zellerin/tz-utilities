db159b225618bde70224250906dcf1b7b8b91a31
Tomáš Zellerin <tomas@zellerin.cz>
2023-09-20 11:03:08 +0200

    Add day-before and fix tomorrow.

--- time.lisp
+++ time.lisp
@@ -4,7 +4,8 @@
   "Time related utilities. The baseline and convention is the LOCAL-TIME package."
   (tomorrow)
   (*org-time-format* variable)
-  (local-time readtable))
+  (local-time readtable)
+  (day-before))
 
 (named-readtables:defreadtable local-time
   "@ reads timestring, #@ universal time."
@@ -12,14 +13,17 @@
   (:macro-char #\@ 'local-time::%read-timestring)
   (:dispatch-macro-char #\# #\@ 'local-time::%read-universal-time))
 
-(defun tomorrow (&optional (n 0) (base (now)))
-  "Timestamp of Nth day in future (0 being tomorrow) 00:00."
+(defun tomorrow (&optional (n 1) (base (now)))
+  "Timestamp of Nth day in future (1 being tomorrow) 00:00."
   (let ((result (local-time::clone-timestamp base)))
     (setf (sec-of result) 0)
     (setf (nsec-of result) 0)
-    (incf (day-of result) (1+ n))
+    (incf (day-of result) n)
     result))
 
 (defvar *org-time-format*
   '(:year "-" (:month 2) "-" (:day 2) " " (:hour 2) ":" (:min 2) " " :short-weekday)
   "Format of timestamp used by Org mode.")
+
+(defun day-before (date &optional (days 1))
+  (local-time:adjust-timestamp date (offset :day (- days))))
